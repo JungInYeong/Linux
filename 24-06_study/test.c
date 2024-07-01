12244fe523650e42930847fd4dd3df06e08a287f
JungInYeong <167291569+JungInYeong@users.noreply.github.com>
2024-07-01 17:36:38 +0900

    C.4

---
+++ 24-06_study/test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+	char a = 8;
+	char b = 4;
+	char c = 0;
+
+	// 비트 연산자
+
+	printf("%d\n", a | b);
+	printf("%d\n", a & b);
+	printf("%d\n", a << 1);
+	printf("%d\n", a >> 1);
+}
